b458cad64271a557ea7ebe516a4e72c12c7c6ccc
l1005551_galicia <braian.torres@bancogalicia.com.ar>
2023-12-21 12:37:39 -0300

    Initial Commit

---
+++ ControllerApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ControllerApp
+//
+//  Created by Rodrigo Torres on 21/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ControllerApp/ControllerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllerAppApp.swift
+//  ControllerApp
+//
+//  Created by Rodrigo Torres on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ControllerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

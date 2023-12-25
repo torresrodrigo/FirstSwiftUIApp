1854182ce9e610bdafdcf4f78b9908a4a5fc0da7
l1005551_galicia <braian.torres@bancogalicia.com.ar>
2023-12-25 17:17:08 -0300

    first commit

---
+++ ControllerApp/Model/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  ControllerApp
+//
+//  Created by Rodrigo Torres on 25/12/2023.
+//
+
+import Foundation
---
+++ ControllerApp/View/TextViews.swift
@@ -0,0 +1,20 @@
+//
+//  TextViews.swift
+//  ControllerApp
+//
+//  Created by Rodrigo Torres on 25/12/2023.
+//
+
+import SwiftUI
+
+struct TextViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextViews_Previews: PreviewProvider {
+    static var previews: some View {
+        TextViews()
+    }
+}
